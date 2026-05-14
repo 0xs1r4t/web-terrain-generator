101f52c4e9b7843f2b46b7716bf49c66d240f0d9
Sirat <sirat.b99@gmail.com>
2026-05-14 22:24:05 +0100

    can read shaders from files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.{glsl,vert,frag,vs,fs}": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(glsl|vert|frag|vs|fs)$/,
+      exclude: /node_modules/,
+      type: "asset/source",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
